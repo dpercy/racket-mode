6c7ff6e1dba498c1af99b961ae6b3f18c17fe5f6
Greg Hendershott <greghendershott@gmail.com>
2015-02-18 08:59:18 -0500

    racket-profile: Various changes:
    - Fix byte compile error from previous commit.
    - Highlight in *Racket Profile* window, too.
    - Only run in racket-mode.
    - Fine tune column widths and enable truncate-lines.
    - Remove gratuitous use of dash.el.

--- racket-profile.el
+++ racket-profile.el
@@ -17,7 +17,6 @@
 ;; http://www.gnu.org/licenses/ for details.
 
 (require 'cl-lib)
-(require 'dash)
 (require 'racket-edit)
 
 (defvar racket--profile-results nil)
@@ -26,63 +25,80 @@
 (defun racket-profile ()
   "Run with profiling enabled. Navigate results."
   (interactive)
-  (racket--do-run 'profile)
-  (setq racket--profile-results (racket--eval/sexpr ",get-profile"))
-  (setq racket--profile-sort-col 1)
-  (with-current-buffer (get-buffer-create "*Racket Profile*")
-    (racket-profile-mode)
-    (racket--profile-sort)
-    (pop-to-buffer (current-buffer))))
+  (when (eq major-mode 'racket-mode)
+    (racket--do-run 'profile)
+    (setq racket--profile-results (racket--eval/sexpr ",get-profile"))
+    (setq racket--profile-sort-col 1)
+    (with-current-buffer (get-buffer-create "*Racket Profile*")
+      (racket-profile-mode)
+      (racket--profile-sort)
+      (pop-to-buffer (current-buffer)))))
 
 (defun racket--profile-sort ()
   "Toggle sort between Count and Time."
   (interactive)
   (read-only-mode -1)
   (erase-buffer)
+  (setq truncate-lines t) ;let run off right edge
   (setq racket--profile-sort-col
         (cl-case racket--profile-sort-col
           (0 1)
           (t 0)))
   ;; TODO: Would be nice to set the Count and Time column widths based
   ;; on max values.
-  (let ((xss (sort (cl-copy-list racket--profile-results)
-                    (lambda (a b) (> (nth racket--profile-sort-col a)
-                                     (nth racket--profile-sort-col b))))))
-    (dolist (xs xss)
-      ;; TODO: Insert property with file and locations, for RET to use.
-      (insert (propertize (apply #'format "%8d %6d %-20s %s\n" xs)
-                          'racket-profile-location
-                          (cl-cdddr xs)))))
-  (setq header-line-format (format " %8s %6s %-20s %s"
-                                   "Count" "Time" "Name" "File"))
+  (setq header-line-format
+        (format " %8s %6s %-20.20s %s"
+                (if (= 0 racket--profile-sort-col) "COUNT" "Count")
+                (if (= 1 racket--profile-sort-col) "TIME" "Time")
+                "Name"
+                "File"))
+  (insert (mapconcat (lambda (xs)
+                       (cl-destructuring-bind (count time name file beg end) xs
+                         (propertize (format "%8d %6d %-20.20s %s"
+                                             count time name file)
+                                     'racket-profile-location
+                                     (list file beg end))))
+                     (sort (cl-copy-list racket--profile-results)
+                           (lambda (a b) (> (nth racket--profile-sort-col a)
+                                            (nth racket--profile-sort-col b))))
+                     "\n"))
   (read-only-mode 1)
   (goto-char (point-min)))
 
-(defvar racket--profile-visit-overlay nil)
+(defvar racket--profile-overlay-this nil)
+(defvar racket--profile-overlay-that nil)
 
 (defun racket--profile-visit ()
   (interactive)
-  (let ((win (selected-window)))
-    (-let [(file beg end) (get-text-property (point) 'racket-profile-location)]
-      (find-file-other-window file)
-      (setq racket--profile-visit-overlay (make-overlay beg end (current-buffer)))
-      (overlay-put racket--profile-visit-overlay 'face 'next-error)
-      (add-hook 'pre-command-hook #'racket--profile-remove-overlay)
-      (select-window win))))
+  (let ((win  (selected-window))
+        (prop (get-text-property (point) 'racket-profile-location)))
+    (when prop
+      (setq racket--profile-overlay-this
+            (make-overlay (save-excursion (beginning-of-line) (point))
+                          (save-excursion (end-of-line) (point))
+                          (current-buffer)))
+      (overlay-put racket--profile-overlay-this 'face 'next-error)
+      (cl-destructuring-bind (file beg end) prop
+        (find-file-other-window file)
+        (setq racket--profile-overlay-that (make-overlay beg end (current-buffer)))
+        (overlay-put racket--profile-overlay-that 'face 'next-error)
+        (add-hook 'pre-command-hook #'racket--profile-remove-overlay)
+        (select-window win)))))
+
+(defun racket--profile-remove-overlay ()
+  (delete-overlay racket--profile-overlay-this)
+  (delete-overlay racket--profile-overlay-that)
+  (remove-hook 'pre-command-hook #'racket--profile-remove-overlay))
 
 (defun racket--profile-next ()
   (interactive)
-  (forward-line)
+  (forward-line 1)
   (racket--profile-visit))
 
 (defun racket--profile-prev ()
   (interactive)
-  (previous-line)
+  (forward-line -1)
   (racket--profile-visit))
-
-(defun racket--profile-remove-overlay ()
-  (delete-overlay racket--profile-visit-overlay)
-  (remove-hook 'pre-command-hook #'racket--profile-remove-overlay))
 
 (defun racket--profile-quit ()
   (interactive)
